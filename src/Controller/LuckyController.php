d53db86b9dc260ec69ff4b0b5f8fd38a6bfec930
erickb21 <erick.beaugendre@gmail.com>
2018-06-25 16:55:59 +0200

    commit 2 - structure et icons

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -11,15 +11,22 @@ use Symfony\Component\HttpFoundation\Response;
 	 * @version 1.0
 	 * @author Stagiaire
 	 */
-class LuckyController
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class LuckyController extends Controller
 {
+    /**
+     * @Route("/lucky/number")
+     */
     public function number()
     {
-        $number = mt_rand(0, 100);
+        $number = random_int(0, 100);
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+        return $this->render('lucky/number.html.twig', array(
+            'number' => $number,
+        ));
     }
 }
 
